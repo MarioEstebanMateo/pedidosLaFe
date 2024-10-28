f5412adcafdaf11955d753f220e302acf05bbd4c
Mario Esteban Mateo <memateo@gmail.com>
2024-10-28 15:35:23 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-center bg-blue-500 text-white p-4">Hello, Tailwind CSS!</h1>
+    </>
+  )
+}
+
+export default App
