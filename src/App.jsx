0430a6a874c5194e57592cd18f7f9f9cc1b6dc20
Mario Esteban Mateo <memateo@gmail.com>
2025-06-02 13:14:37 -0300

    added footer to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { OrderProvider } from './context/OrderContext'
 import Home from '../src/components/Home'
 import ReviewOrder from '../src/components/ReviewOrder'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/review" element={<ReviewOrder />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </OrderProvider>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+
+const Footer = () => {
+  return (
+    <>
+    <div>
+        <footer className="bg-gray-800 text-white py-4">
+            <div className="container mx-auto text-center">
+                <p className="text-base">
+                    &copy; {new Date().getFullYear()} - La Fe S.A. - Todos los derechos reservados.
+                </p>
+                <p className="text-base mt-2">
+                    Diseñada y Programada por <a href="mailto:memateo@gmail.com" className="text-blue-400 hover:underline">
+                        Mario E. Mateo
+                    </a> para La Fe S.A.</p>
+            </div>
+        </footer>
+    </div>
+    </>
+  )
+}
+
+export default Footer
